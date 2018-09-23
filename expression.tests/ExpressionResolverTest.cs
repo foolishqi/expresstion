7e206c908bfd1aadbb9509c7cb53fff31f66c874
foolishqi <foolishqi@126.com>
2018-09-23 16:27:14 +0800

    add expression resolver and test;

--- expression.tests/ExpressionResolverTest.cs
+++ expression.tests/ExpressionResolverTest.cs
@@ -10,55 +10,136 @@ namespace expression.tests
 
         public ExpressionResolverTest()
         {
+            this.resolver = new ExpressionResolver();
         }
 
         [Fact]
         public void EmptyTest()
         {
-            resolver = new ExpressionResolver("");
+            var expressions = resolver.Resolve("");
 
-            Assert.Empty(resolver.OperandsStack);
+            Assert.Empty(expressions);
         }
 
         [Fact]
-        public void ConstTest()
+        // 数值常量测试
+        public void NumberConstTest()
         {
-            resolver = new ExpressionResolver("1");
+            var expressions = resolver.Resolve("123");
 
-            Assert.Collection(resolver.OperandsStack,
-                x => Assert.Equal("1", x));
+            Assert.Collection(expressions,
+                x => Assert.Equal("123", x));
         }
 
         [Fact]
-        public void AnotherConstTest()
+        // 字符串常量测试
+        public void StringConstTest()
         {
-            resolver = new ExpressionResolver("2");
+            var expressions = resolver.Resolve("\"hello world.\"");
 
-            Assert.Collection(resolver.OperandsStack,
-                x => Assert.Equal("2", x));
+            Assert.Collection(expressions,
+                x => Assert.Equal("\"hello world.\"", x));
         }
 
         [Fact]
-        public void MultiConstTest()
+        // 算术运算测试
+        public void ArithmeticOperationTest()
         {
-            resolver = new ExpressionResolver("1 2 3");
+            var expressions = resolver.Resolve("1 + 1");
 
-            Assert.Collection(resolver.OperandsStack,
-                x => Assert.Equal("3", x),
+            Assert.Equal(3, expressions.Count);
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("+", x));
+        }
+
+        [Fact]
+        // 优先级测试
+        public void PrecedenceTest()
+        {
+            var expressions = resolver.Resolve("1 + 2 * 3 - 4");
+
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
                 x => Assert.Equal("2", x),
-                x => Assert.Equal("1", x));
+                x => Assert.Equal("3", x),
+                x => Assert.Equal("*", x),
+                x => Assert.Equal("+", x),
+                x => Assert.Equal("4", x),
+                x => Assert.Equal("-", x));
         }
 
-        // [Fact]
-        // public void OnePlusOneTest()
-        // {
-        //     resolver = new ExpressionResolver("1 + 1");
+        [Fact]
+        // 结合性测试
+        public void AssociativityTest()
+        {
+            var expressions = resolver.Resolve("1 + 2 - 3 + 4");
 
-        //     Assert.Equal(3, resolver.OperandsStack.Count);
-        //     Assert.Collection(resolver.OperandsStack,
-        //         x => Assert.Equal("1", x),
-        //         x => Assert.Equal("+", x),
-        //         x => Assert.Equal("1", x));
-        // }
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("2", x),
+                x => Assert.Equal("+", x),
+                x => Assert.Equal("3", x),
+                x => Assert.Equal("-", x),
+                x => Assert.Equal("4", x),
+                x => Assert.Equal("+", x));
+        }
+
+        [Fact]
+        // 括号测试
+        public void ParenthesisTest()
+        {
+            var expressions = resolver.Resolve("1 * (2 + 3)");
+
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("2", x),
+                x => Assert.Equal("3", x),
+                x => Assert.Equal("+", x),
+                x => Assert.Equal("*", x));
+        }
+
+        [Fact]
+        // 嵌套括号测试
+        public void NestedParenthesisTest()
+        {
+            var expressions = resolver.Resolve("(1 + 2) * ((3 + 4) - 5)");
+
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("2", x),
+                x => Assert.Equal("+", x),
+                x => Assert.Equal("3", x),
+                x => Assert.Equal("4", x),
+                x => Assert.Equal("+", x),
+                x => Assert.Equal("5", x),
+                x => Assert.Equal("-", x),
+                x => Assert.Equal("*", x));
+        }
+
+        [Fact]
+        // 函数测试
+        public void FunctionTest()
+        {
+            var expressions = resolver.Resolve("Add(1, 2)");
+
+            Assert.Collection(expressions,
+                x => Assert.Equal("1", x),
+                x => Assert.Equal("2", x),
+                x => Assert.Equal("Add", x));
+        }
+
+        [Fact]
+        // 嵌套函数测试
+        public void NestedFunctionTest()
+        {
+            var expressions = resolver.Resolve("Datediff(\"2018-10-01\", Now())");
+
+            Assert.Collection(expressions,
+                x => Assert.Equal("\"2018-10-01\"", x),
+                x => Assert.Equal("Now", x),
+                x => Assert.Equal("Datediff", x));
+        }
     }
 }
--- expression/ExpressionResolver.cs
+++ expression/ExpressionResolver.cs
@@ -1,60 +1,172 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace expression
 {
     public class ExpressionResolver
     {
-        private string text;
-        private Stack<String> operandsStack;
-        private Stack<String> operationsStack;
-
-        public ExpressionResolver(string text)
+        public ExpressionResolver()
         {
-            this.text = text;
-            this.operandsStack = new Stack<String>();
-            this.operationsStack = new Stack<String>();
-
-            this.Resolve();
         }
 
-        public Stack<String> OperandsStack => this.operandsStack;
-
-        public void Resolve()
+        public List<String> Resolve(string text)
         {
-            if (!string.IsNullOrEmpty(this.text))
+            ExpressionReader reader = new ExpressionReader(text);
+
+            // 1. 初始化一个空堆栈，将结果字符串变量置空。
+            Stack<String> operatorsStack = new Stack<String>();
+            List<String> expressions = new List<String>();
+
+            string item = string.Empty;
+            // 2. 从左到右读入中缀表达式，每次一个字符。
+            while ((item = reader.Read()) != string.Empty)
             {
-                foreach (var item in this.text.Split(' '))
+                var type = GetExpressionType(item);
+
+                // 3. 如果字符是操作数，将它添加到结果字符串。
+                if (type == ExpressionType.Operand)
                 {
-                    this.operandsStack.Push(item);
+                    expressions.Add(item);
+                }
+                else if (type == ExpressionType.Operators || type == ExpressionType.Function)
+                {
+                    // 5. 如果字符是个开括号，把它压入堆栈。
+                    if (IsOpeningParenthesis(item))
+                    {
+                        operatorsStack.Push(item);
+                    }
+                    else
+                    {
+                        var precedenceOfCurrent = GetOperatorPrecedence(item);
+
+                        while (operatorsStack.Count > 0)
+                        {
+                            var operatorOnStack = operatorsStack.Peek();
+                            var precedenceOfStack = GetOperatorPrecedence(operatorOnStack);
+
+                            if (
+                                // 4. 如果字符是个操作符，弹出（pop）操作符，直至遇见开括号（opening parenthesis）、
+                                //    优先级较低的操作符或者同一优先级的右结合符号。把这个操作符压入（push）堆栈。
+                                !(IsOpeningParenthesis(operatorOnStack) || precedenceOfCurrent < precedenceOfStack) ||
+                                // 6. 如果字符是个闭括号（closing parenthesis），在遇见开括号前，弹出所有操作符，
+                                //    然后把它们添加到结果字符串。
+                                (IsClosingParenthesis(item) && !IsOpeningParenthesis(operatorOnStack))
+                            )
+                            {
+                                expressions.Add(operatorsStack.Pop());
+                            }
+                            else
+                            {
+                                if (IsClosingParenthesis(item) && IsOpeningParenthesis(operatorOnStack))
+                                {
+                                    operatorsStack.Pop();
+                                }
+                                break;
+                            }
+                        }
+
+                        if (!IsClosingParenthesis(item))
+                        {
+                            operatorsStack.Push(item);
+                        }
+                    }
                 }
             }
 
-            // StringReader reader = new StringReader(this.text);
-            // var currentItem = string.Empty;
-            // char currentChar;
+            // 7. 如果到达输入字符串的末尾，弹出所有操作符并添加到结果字符串。
+            foreach (var operators in operatorsStack)
+            {
+                expressions.Add(operators);
+            }
 
-            // do
-            // {
-            //     currentChar = (char)reader.Read();
+            return expressions;
+        }
 
-            //     if (currentChar == '+')
-            //     {
-            //         this.operationsStack.Push(currentChar);
-            //     }
-            // } while (currentChar != -1);
+        private ExpressionType GetExpressionType(string input)
+        {
+            if (Regex.IsMatch(input, @"(?:-?\d+\.?\d*)|(?:\"".*?\"")"))
+            {
+                return ExpressionType.Operand;
+            }
+            else if (Regex.IsMatch(input, @"(?:\(|\)|!|\*|/|%|\+|-|<=?|>=?|==|!=|&|\|)"))
+            {
+                return ExpressionType.Operators;
+            }
+            else if (Regex.IsMatch(input, @"(?:[A-Za-z_]+)"))
+            {
+                return ExpressionType.Function;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(input), input, "未知的表达式类型."
+                );
+            }
+        }
 
-            // if (!string.IsNullOrEmpty(text))
-            // {
-            //     this.OperandsStack.Push("1");
+        private int GetOperatorPrecedence(string operators)
+        {
+            int precedence;
 
-            //     if (text.Contains("+"))
-            //     {
-            //         this.OperandsStack.Push("+");
-            //         this.OperandsStack.Push("1");
-            //     }
-            // }
+            switch (operators)
+            {
+                case "(":
+                case ")":
+                    precedence = 1;
+                    break;
+                case "!":
+                    precedence = 2;
+                    break;
+                case "*":
+                case "/":
+                case "%":
+                    precedence = 3;
+                    break;
+                case "+":
+                case "-":
+                    precedence = 4;
+                    break;
+                case "<":
+                case "<=":
+                case ">":
+                case ">=":
+                    precedence = 6;
+                    break;
+                case "==":
+                case "!=":
+                    precedence = 7;
+                    break;
+                case "&":
+                    precedence = 11;
+                    break;
+                case "|":
+                    precedence = 12;
+                    break;
+                default:
+                    precedence = 20;
+                    break;
+            }
+
+            return precedence;
+        }
+
+        private bool IsOpeningParenthesis(string item)
+        {
+            return "(".Equals(item);
+        }
+
+        private bool IsClosingParenthesis(string item)
+        {
+            return ")".Equals(item);
+        }
+
+        public enum ExpressionType
+        {
+            Operand,
+            Operators,
+            Function
         }
     }
 }
